3712c557a833ec4638262fbc9f3b920f98ee1d8d
consen <1186508815@qq.com>
2015-02-03 12:06:00 +0800

    leetcode 172 excel sheet column number.

---
+++ 171_excel_sheet_column_number/title2number.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @param s, a string
+    # @return an integer
+    def titleToNumber(self, s):
+        num = 0
+        s = s[::-1]
+        for i in range(0, len(s)):
+            num += (ord(s[i]) - ord('A') + 1) * (26 ** i)
+        return num
+
+def main():
+    s = Solution()
+    print s.titleToNumber('A')
+    print s.titleToNumber('Z')
+    print s.titleToNumber('AA')
+    print s.titleToNumber('AB')
+
+if __name__ == '__main__':
+    main()
+

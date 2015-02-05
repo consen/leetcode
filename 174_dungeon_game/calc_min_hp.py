e9947b36bfbdd4f75f7d081da0ef883142b3a7da
consen <1186508815@qq.com>
2015-02-05 23:19:56 +0800

    174 dungeon game.

--- 174_dungeon_game/calc_min_hp.py
+++ 174_dungeon_game/calc_min_hp.py
@@ -1,20 +1,34 @@
-class Solution:
+class Solution: # Time Limit Exceeded
     # @param dungeon, a list of lists of integers
     # @return a integer
     def calculateMinimumHP(self, dungeon):
+        """
+        core: the minimun hp to go to next point
+        """
+        if len(dungeon) == 1 and len(dungeon[0]) == 1:  # one point
+            return 1 if dungeon[0][0] >=0 else -dungeon[0][0] + 1
+        elif len(dungeon) > 1 and len(dungeon[0]) == 1: # one column
+            hp_down = self.calculateMinimumHP(dungeon[1:])
+            min_hp = hp_down - dungeon[0][0]
+            return 1 if min_hp <= 0 else min_hp
+        elif len(dungeon) == 1 and len(dungeon[0]) > 1: # one row
+            hp_right = self.calculateMinimumHP([line[1:] for line in dungeon])
+            min_hp = hp_right - dungeon[0][0]
+            return 1 if min_hp <= 0 else min_hp
+        elif len(dungeon) > 1 and len(dungeon[0]) > 1:
+            hp_down = self.calculateMinimumHP(dungeon[1:])
+            hp_right = self.calculateMinimumHP([line[1:] for line in dungeon])
+            min_hp = min(hp_down, hp_right) - dungeon[0][0]
+            return 1 if min_hp <= 0 else min_hp
+        
+        
+def main():
+    m = [[-2, -3, 3],
+         [-5, -10, 2],
+         [10, 30, -5]]
+    s = Solution()
+    print s.calculateMinimumHP(m)
+    print s.calculateMinimumHP([[5, -5]])
 
-        hp_right = calculateMinimumHP()
-        hp_down = calculateMinimumHP()
-        
-        ans = 1
-        if (dungeon[0][0] < 0)
-            ans = -dungeon[0][0] + 1
-        
-        if hp_right < hp_down < 0:
-            return ans + (-hp_down) + 1
-        if hp_down < hp_right < 0:
-            return ans + (-hp_right) + 1
-
-        if hp_right >= 0 or hp_down > = 0
-            return ans + 1
-        
+if __name__ == '__main__':
+    main()
